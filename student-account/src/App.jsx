c4c14469be1ab5e6d2fff6a01eb57c5b7a18264b
Sofya Sidonskaya <sonyasidonya@gmail.com>
2025-03-31 18:17:53 +0700

    Route
    changed route for login page

--- student-account/src/App.jsx
+++ student-account/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
     <ThemeProvider>
       <Routes>
-        <Route path="/" element={<LoginPage />} /> 
+        <Route path="/login" element={<LoginPage />} /> 
       </Routes>
     </ThemeProvider>
     </>
